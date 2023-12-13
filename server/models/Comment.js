c0a77dce43f60eb9114deed4518f3afe88b2cc76
Blagovest Stoilov <108930312+alexstoilov@users.noreply.github.com>
2023-12-13 16:04:56 +0200

    Implemented basic header / footer.

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,15 +1,14 @@
-const { Schema, model, Types } = require("mongoose");
+const {Schema, model, Types} = require("mongoose");
 
 const commentSchema = new Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  lastEdit: { type: Date, default: Date.now },
-  authorName: { type: String, required: true },
-  author: { type: Types.ObjectId, ref: "User" },
-  article: { type: Types.ObjectId, ref: "Article" },
-  usersLiked: { type: [Types.ObjectId], ref: "User", default: [] },
-});
+        title: {type: String, required: true},
+        content: {type: String, required: true},
+        authorName: {type: String, required: true},
+        author: {type: Types.ObjectId, ref: "User"},
+        article: {type: Types.ObjectId, ref: "Article"},
+        usersLiked: {type: [Types.ObjectId], ref: "User", default: []},
+    }, {timestamps: true}
+);
 
 const Comment = model("Comment", commentSchema);
 module.exports = Comment;
